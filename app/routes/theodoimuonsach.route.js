a0e0d31a246d3db812760804163c1060b62db5ae
Le Nhat Minh <minhb2111809@student.ctu.edu.vn>
2025-03-19 10:27:21 +0700

    Thêm controller Theo dõi mượn sách với các xử lý phê duyệt yêu cầu mượn

---
+++ app/routes/theodoimuonsach.route.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const theodoimuonsach = require("../controllers/theodoimuonsach.controller");
+const { authJwt } = require("../middlewares");
+
+const router = express.Router();
+
+// Routes for any logged-in user
+router.get("/", [authJwt.verifyToken], theodoimuonsach.findAll);
+router.get("/:id", [authJwt.verifyToken], theodoimuonsach.findOne);
+router.post("/", [authJwt.verifyToken], theodoimuonsach.create);
+
+// Routes requiring admin privileges
+router.put(
+  "/:id",
+  [authJwt.verifyToken, authJwt.isAdmin],
+  theodoimuonsach.update
+);
+router.delete(
+  "/:id",
+  [authJwt.verifyToken, authJwt.isAdmin],
+  theodoimuonsach.delete
+);
+router.delete(
+  "/",
+  [authJwt.verifyToken, authJwt.isAdmin],
+  theodoimuonsach.deleteAll
+);
+
+module.exports = router;
